7bdaaad42233657ec7f334964d48e3363afe8319
seoyoung <sy2027j@naver.com>
2024-07-29 16:15:33 +0900

    feat : add VueController

---
+++ src/main/java/com/project/dailylog/controller/VueController.java
@@ -0,0 +1,19 @@
+package com.project.dailylog.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VueController {
+
+    private static final Logger logger = LoggerFactory.getLogger(VueController.class);
+
+    @RequestMapping(value = { "/", "/**/{path:[^\\.]*}" })
+    public String index(HttpServletRequest request) {
+        logger.info("Request URL: {}", request.getRequestURL());
+        return "/vueStatic/index.html";
+    }
+}
